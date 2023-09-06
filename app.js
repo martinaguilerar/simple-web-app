2097ac21c7133645840c918988fe753b7764d2e1
martinaguilerar <martin.aguileraromero@gmail.com>
2023-09-05 20:07:48 -0600

    Modify port to 8080

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const config = {
     }
 }
   
-app.listen(8081, () => {
-  console.log(`Server is up and running on 8081 ...`);
+app.listen(8080, () => {
+  console.log(`Server is up and running on 8080 ...`);
 });
 
 app.get("/results", async (req, res) => {
